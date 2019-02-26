502f90705da12a1be38ca278fa2a1d5919b2831b
Geoff Safcik <geoffs@soundstrue.com>
2019-02-26 15:09:26 -0700

    Added Unary (request/response) API

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/gsafcik/grpc-go-course/greet/greetpb"
 	"google.golang.org/grpc"
@@ -9,6 +10,18 @@ import (
 )
 
 type server struct{}
+
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	fmt.Printf("Greet function was invoked with %v\n", req)
+	firstName := req.GetGreeting().GetFirstName()
+
+	result := "Hello " + firstName
+	res := &greetpb.GreetResponse{
+		Result: result,
+	}
+
+	return res, nil
+}
 
 func main() {
 	fmt.Println("Hello world")
