63f9fa19cfa58a8e0d6e7a561b36c5bf93dce445
Geoff Safcik <geoffs@soundstrue.com>
2019-03-01 14:29:04 -0700

    Added reflection to calculator service in order to use evans CLI

--- calculator/calculator_server/server.go
+++ calculator/calculator_server/server.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/gsafcik/grpc-go-course/calculator/calculatorpb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"google.golang.org/grpc/status"
 )
 
@@ -127,6 +128,9 @@ func main() {
 
 	s := grpc.NewServer()
 	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
+
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to server: %v", err)
